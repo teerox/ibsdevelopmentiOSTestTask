bd17c90e8a761fda56d8d2a6a68375ba52c767e0
Mac <mac@Teerox.lan>
2022-10-02 10:19:53 +0100

    first commit

---
+++ IBSdevelopmentiOSTestTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IBSdevelopmentiOSTestTask
+//
+//  Created by Mac on 02/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IBSdevelopmentiOSTestTask/IBSdevelopmentiOSTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBSdevelopmentiOSTestTaskApp.swift
+//  IBSdevelopmentiOSTestTask
+//
+//  Created by Mac on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct IBSdevelopmentiOSTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
