31c53d3a9d299e056e1e8ce2de7b942f482587a8
Mac <mac@Teerox.lan>
2022-10-03 11:24:02 +0100

    set login view as first page to be displayed

--- IBSdevelopmentiOSTestTask/ContentView.swift
+++ IBSdevelopmentiOSTestTask/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            if !viewModel.loginSuccess {
+            if viewModel.loginSuccess {
                 Bodyview(viewModel: viewModel)
             } else {
                 Loginview(viewModel: viewModel)
